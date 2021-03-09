d48827074f826dce2fe9306fc7682798e961dd3b
Ivan Morett <luisivanmorett@gmail.com>
2021-03-09 16:52:52 -0600

    graphql and simple login/signup/logout

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstConnection from './Components/FirstConnection/FirstConnection'
+import { ApolloProvider } from '@apollo/client';
+import GraphQLClient from './utils/GraphQLClient';
+import { useState } from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token') != null);
+  const logOut = () => {
+    localStorage.clear();
+    GraphQLClient.resetStore();
+    setLoggedIn(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={GraphQLClient}>
+      {loggedIn ?
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+      </p>
+            <a onClick={logOut} href="#">LOGOUT</a>
+          </header>
+        </div> : <FirstConnection setLoggedIn={setLoggedIn} />}
+    </ApolloProvider>
   );
 }
 
---
+++ extension/src/Components/FirstConnection/Login.jsx
@@ -0,0 +1,48 @@
+import { useLazyQuery, gql } from '@apollo/client';
+import { useState } from 'react';
+
+const LOGIN = gql`query login($user: LoginInput!){
+  login(user: $user) {
+    user{
+      settings {
+        searchEngine,
+        googleToken,
+        backgroundImage
+      }
+    },
+    jwt
+  }
+}`
+
+function LogIn({ setLoggedIn }) {
+  const [username, setUsername] = useState("test");
+  const [password, setPassword] = useState("test");
+
+  //GRAPHQL
+  const [login, { called, error, loading, data }] = useLazyQuery(LOGIN)
+
+  if (called && !loading && data && !error) {
+    localStorage.setItem("token", data.login.jwt)
+    localStorage.setItem("settings", JSON.stringify(data.login.user.settings))
+    setLoggedIn(true)
+  }
+
+  return (
+    <div className="login">
+      {error ? <>{error.message}</> : <></>}
+      {loading ? "..." : <div>
+        <input type="text" name="user" value={username} onChange={(ev) => setUsername(ev.target.value)} />
+        <input type="text" name="password" value={password} onChange={(ev) => setPassword(ev.target.value)} />
+        <input type="button" value="login" onClick={() => {
+          login({
+            variables: { user: { username, password } }
+          })
+        }} />
+      </div>
+      }
+
+    </div>
+  );
+}
+
+export default LogIn;
