e2aae6da7d67f6c57499048feb83586b2cee2836
AlexGz9851 <a00820225@itesm.mx>
2021-03-10 11:36:24 -0600

    Changed Note Component to Hooks syntax.

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Note from './Notes/Note.jsx'
 
+let text = "Hacer esta libreria de notas!"; 
+function coolFunction(id){
+  console.log("HOLA"+ id);
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Note id='1' title='Tarea cool!' content='Lavar ropa'  posX='300px' onRemove={(e,a) =>coolFunction(e, a)}/>
+        <Note id='2' title='Tarea Web' content={text}  posX='100px' posY='260px' onRemove={(e,a) =>coolFunction(e, a)}/>
+        <Note id='3' title='Tarea cool!' content='Lavar ropa' onRemove={(e,a) =>coolFunction(e, a)}  />
+        <Note id='5' title='Tarea cool!' content='Lavar ropa' onRemove={(e,a) =>coolFunction(e, a)}   />
+        <Note id='6' title='Tarea cool!' content='Lavar ropa' onRemove={(e,a) =>coolFunction(e, a)}  />
+        <Note id='7' title='Tarea cool!' content='Lavar ropa' onRemove={(e,a) =>coolFunction(e, a)}   />
+        <Note id='8' title='Tarea cool!' content='Lavar ropa' onRemove={(e,a) =>coolFunction(e, a)}  />
+        <Note id='4' title='Frase del dia' content='te quiero mucho bb porque el deseo del alma es soportar un poco mas'  posX='100px' onRemove={(e,a) =>coolFunction(e, a)}/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- extension/src/Notes/Note.jsx
+++ extension/src/Notes/Note.jsx
@@ -1,51 +1,65 @@
-import React from "react";
-import './Note.css'
+import React, {useState} from "react";
+import './Note.css';
 
 
-class Note extends React.Component {
-  constructor(props) {
-    super(props);
-    this.id = props.id;
+function Note(props){
+  const [title, setTitle] = useState(props.title);
+  const [id, setId] = useState(props.id);
+  const [content, setContent] = useState(props.content);
+  const [posX, setPosX] = useState(props.posX);
+  const [posY, setPosY] = useState(props.posY);
 
-    this.getIdTag = this.getIdTag.bind(this);
-    this.setContentText = this.setContentText.bind(this);
-    this.setTitleText = this.setTitleText.bind(this);
-
-    this.state = {title:this.props.title, content:this.props.content,};
-    this.textColors = ["#ccc","#d7b2FF", "#FBD5EC","#A7DDFA","#eeffee","#41402E","#FFEDC0"]
-    this.backgroundColors = ["#333","#442666","#7A4364","#2D5F7A","#2FA853","#E6E34C",'#8F4623']
-    this.headerColors = ["#444","#695580","#C76DA3","#4897C2","#5EA86C","#ADAC51","#E68550"]
-  }
-
-  getIdTag(subsection){
-    return "nota" + this.id + subsection;
-  }
-
-  getObj(id){
-    return document.getElementById(id);
-  }
-
-  setContentText(event, elmnt){
-    this.setState({content:event.target.value});
-    if (typeof this.props.onTitleChanged !== 'undefined'){
-      this.props.onTitleChanged(event, elmnt);
-    }
-  }
   
-  setTitleText(event, elmnt){
-    this.setState({title:event.target.value});
-    if (typeof this.props.onTitleChanged !== 'undefined'){
-      this.props.onTitleChanged(event, elmnt);
+  let textColors = ["#ccc","#d7b2FF", "#FBD5EC","#A7DDFA","#eeffee","#41402E","#FFEDC0"];
+  let backgroundColors = ["#333","#442666","#7A4364","#2D5F7A","#2FA853","#E6E34C",'#8F4623'];
+  let headerColors = ["#444","#695580","#C76DA3","#4897C2","#5EA86C","#ADAC51","#E68550"];
+
+  let defX = ""+(window.innerWidth- 200)+"px";
+  let newPosX = (typeof posX === 'undefined') ? defX : posX; 
+  let newPosY = (typeof posY === 'undefined') ? "50px": posY;
+  let intId = (5+ parseInt(id, 10)) % backgroundColors.length;
+
+  const noteStyle = {
+    top:newPosY,
+    left:newPosX,
+    backgroundColor: backgroundColors[intId],
+  };
+
+  const headerStyle = {
+    backgroundColor: headerColors[intId],
+    color:textColors[intId],
+  };
+
+  const contentStyle = {
+    backgroundColor: backgroundColors[intId],
+    color:textColors[intId],
+  };
+  
+  const handleChangeContent = (e) =>{
+    e.preventDefault();
+    setContent(e.target.value);
+    if (typeof props.onChangeContent !== 'undefined'){
+      props.onChangeContent(id);
+    }
+  };
+  
+  const handleChangeTitle= (e) =>{
+    e.preventDefault();
+    setTitle(e.target.value);
+    if (typeof props.onChangeTitle !== 'undefined'){
+      props.onChangeTitle(id);
+    }
+  };
+
+  const handleCloseElement= (e) =>{
+    e.preventDefault();
+    if (typeof props.onRemove !== 'undefined'){
+      props.onRemove(id);
     }
   }
 
-  closeElement(e){
-    if (typeof this.props.onClose !== 'undefined'){
-      this.props.onClose(e);
-    }
-  }
-
-  dragElement(e, elmnt) {
+  const   dragElement = (e)=> {
+    let elmnt = document.getElementById(id);
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     e = e || window.event;
     e.preventDefault();
@@ -75,59 +89,46 @@ class Note extends React.Component {
       document.onmousemove = null;
     }
   }
-    
-    render() {
-      let defX = ""+(window.innerWidth- 200)+"px";
-      let posX = this.props.posX == null ? defX : this.props.posX; 
-      let posY = this.props.posY == null ? "50px": this.props.posY;
-      let intId = (5+ parseInt(this.props.id, 10)) % this.backgroundColors.length;
+
+  return (
+    <div id={id} 
+      className="nota" 
+      style={noteStyle}
+      //ref={this.nota}
+    >
+      <div style={headerStyle} >
+      <div className="nota-drag"  
+        style={headerStyle}
+        id={id+'drag'}
+        onMouseDown={dragElement}
+        ></div>
+      <div className="nota-close"
+        id={id+'close'}
+        onClick={handleCloseElement}
+      >
+      </div>
+      <textarea className='nota-header' type="text" 
+        value={title}
+        id={id+'title'}
+        name={id+'title'}
+        placeholder="Add a cool task here."
+        style={headerStyle}
+        onChange={handleChangeTitle}
+        >
+      </textarea>
+      </div>
+      <textarea className='nota-content' type="text" 
+        value={content}
+        id={id+ 'content'}
+        name={ id +'content'}
+        style={contentStyle}
+        onChange={handleChangeContent}
+        >
+      </textarea>
 
 
-      const noteStyle = {
-        top:posY,
-        left:posX,
-        backgroundColor: this.backgroundColors[intId],
-      };
-
-      const contentStyle = {
-        backgroundColor: this.backgroundColors[intId],
-        color:this.textColors[intId],
-      };
-
-      const headerStyle = {
-        backgroundColor: this.headerColors[intId],
-        color:this.textColors[intId],
-      };
-
-      return ( 
-        <div id={this.getIdTag()} 
-            class="nota" style={noteStyle}
-            ref={this.nota}>
-            <div class="nota-drag"  style={headerStyle}
-                 id={this.getIdTag('drag')}
-                 onMouseDown={(e) => this.dragElement(e, this.getObj(this.getIdTag()))}>
-            </div>
-            <div class="nota-close"
-                 id={this.getIdTag('close')}
-                 onClick={(e) => this.closeElement(e)}>
-            </div>
-            <textarea class='nota-header' type="text" 
-                      value={this.state.title}
-                      id={this.getIdTag('title')}
-                      name={this.getIdTag('title')}
-                      style={headerStyle}
-                      onChange={(e) => this.setTitleText(e, this.getObj(this.getIdTag('title')))}>
-            </textarea>
-            <textarea class='nota-content' type="text" 
-                      value={this.state.content}
-                      id={this.getIdTag('content')}
-                      name={this.getIdTag('content')}
-                      style={contentStyle}
-                      onChange={(e) => this.setContentText(e, this.getObj(this.getIdTag('content')))}>
-            </textarea>
-        </div>
-      );
-    }
+    </div>
+  );
 }
 
 export default Note;
